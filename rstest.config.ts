2b11f96a9fd521b205ae54a2cc42d29140b6a0d8
Eric <meaninglesslegacy@gmail.com>
2026-02-21 23:22:13 -0800

    rsbuild init

---
+++ rstest.config.ts
@@ -0,0 +1,12 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rstest/core';
+
+// Docs: https://rstest.rs/config/
+export default defineConfig({
+  output: {
+    assetPrefix: '/deltavie.github.io/'
+  },
+  plugins: [pluginReact()],
+  testEnvironment: 'happy-dom',
+  setupFiles: ['./tests/rstest.setup.ts'],
+});
